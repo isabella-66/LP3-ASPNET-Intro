0953ccd2c874efb2eec362cd74a13016b229e82e
isabella-66 <amasprog@gmail.com>
2022-08-29 21:08:19 -0300

    fix: conflicts of pull files

--- Controllers/MathOperationsController.cs
+++ Controllers/MathOperationsController.cs
@@ -38,31 +38,18 @@ public class MathOperationsController : Controller
         return View();
     }
 
-<<<<<<< HEAD
     public IActionResult DivOperation([FromForm] Number number)
     {
         return View(number);
     }
 
     public IActionResult Mult()
-=======
-    public IActionResult Sub()
->>>>>>> 3d42094a8cba8d1b05c08086cef828fc8efeebdd
     {
         return View();
     }
 
-<<<<<<< HEAD
     public IActionResult MultOperation([FromForm] Number number)
     {
         return View(number);
-=======
-    public IActionResult SubOperation([FromForm] double numberOne, [FromForm] double numberTwo) 
-    {
-        ViewBag.NumberOne = numberOne;
-        ViewBag.NumberTwo = numberTwo;
-        ViewBag.Result = numberOne - numberTwo;
-        return View();
->>>>>>> 3d42094a8cba8d1b05c08086cef828fc8efeebdd
     }
 }
